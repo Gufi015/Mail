56df7eceff1c6562e0f7b75d4646584253186b9f
Gufi015 <lucras015@hotmail.com>
2018-08-17 15:38:40 -0500

    Proyecto Mail Appc

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,17 @@
-require('ti.cloud');
 
+var Cloud = require('ti.cloud');
 
-$.btnSend.addEventListener(function(e){
+$.btnSend.addEventListener('click',function(e) {
 	Cloud.Emails.send({
-    template: 'welcome',
-    recipients: 'ruf_rm@hotmail.com'
-}, function (e) {
-    if (e.success) {
-        alert('Success');
-    } else {
-        alert('Error:\n' +
-            ((e.error && e.message) || JSON.stringify(e)));
-    }
-});
+		template : 'welcome',
+		recipients : 'ruf_rm@hotmail.com'
+	}, function(e) {
+		if (e.success) {
+			alert('Success');
+		} else {
+			alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+		}
+	});
 });
 
 $.index.open();
